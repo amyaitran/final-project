c4168a6264a3135e3d1c16c3672653875a114051
amyaitran <amyaitran@gmail.com>
2021-11-07 00:34:20 -0700

    add error message on client side for invalid code

--- client/components/mb-player-creation.jsx
+++ client/components/mb-player-creation.jsx
@@ -6,7 +6,8 @@ export default class PlayerCreation extends React.Component {
     super(props);
     this.state = {
       name: null,
-      gameId: null
+      gameId: null,
+      isCodeValid: null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleJoin = this.handleJoin.bind(this);
@@ -36,55 +37,69 @@ export default class PlayerCreation extends React.Component {
 
   initiateMobileSocket() {
     const { name, gameId } = this.state;
-    const mobileSocket = socketIOClient('/mobile', { query: `gameId=${gameId}` });
-    mobileSocket.emit('create player', { name });
+    const socket = socketIOClient('/mobile', { query: `gameId=${gameId}` });
+    socket.emit('create player', { name });
+    socket.on('valid id', valid => {
+      if (valid) {
+        this.setState({ isCodeValid: true });
+      } else {
+        this.setState({ isCodeValid: false });
+      }
+    });
   }
 
   render() {
     return (
-      <>
-        <form onSubmit={this.handleJoin}>
-          <div className="container">
-            <div className="ml-15">
-              <div className="col-full">
-                <label htmlFor="name" className="fs-med fw-semi-bold">name</label>
-              </div>
-            </div>
-            <div className="mb-2">
-              <div className="col-full center">
-                <input
-                onChange={this.handleChange}
-                type="text"
-                name="name"
-                className="shadow"
-                placeholder="enter your name"
-                maxLength="12"
-                required />
-              </div>
-            </div>
-            <div className="ml-15">
-              <div className="col-full">
-                <label htmlFor="gameId" className="fs-med fw-semi-bold">room code</label>
-              </div>
-            </div>
-            <div className="mb-2">
-              <div className="col-full center">
-                <input
-                onChange={this.handleChange}
-                type="text"
-                name="gameId"
-                className="shadow"
-                placeholder="enter 4-letter code"
-                maxLength="4"
-                required />
-              </div>
-            </div>
-            <div className="center">
-              <button className="col-six-tenths shadow">play!</button>
-            </div>
+      (this.state.isCodeValid)
+        ? <div className="center">
+            <button className="height-5"><span className="fw-reg">everybody is in!<br/></span>START</button>
           </div>
-        </form>
-      </>
+        : <>
+          <form onSubmit={this.handleJoin}>
+            <div className="container">
+              <div className="ml-15">
+                <div className="col-full">
+                  <label htmlFor="name" className="fs-med fw-semi-bold">name</label>
+                </div>
+              </div>
+              <div className="mb-2">
+                <div className="col-full center">
+                  <input
+                  onChange={this.handleChange}
+                  type="text"
+                  name="name"
+                  className="shadow"
+                  placeholder="enter your name"
+                  maxLength="12"
+                  required />
+                </div>
+              </div>
+              <div className="ml-15">
+                <div className="col-full">
+                  <label htmlFor="gameId" className="fs-med fw-semi-bold">room code</label>
+                </div>
+              </div>
+              <div className="mb-2">
+                <div className="col-full center">
+                  <input
+                  onChange={this.handleChange}
+                  type="text"
+                  name="gameId"
+                  className="shadow"
+                  placeholder="enter 4-letter code"
+                  maxLength="4"
+                  required />
+                </div>
+                { (this.state.isCodeValid === false)
+                  ? <p className="red bg-yellow center">invalid game code!</p>
+                  : <p className="hidden">invalid game code!</p>}
+              </div>
+              <div className="center">
+                <button className="col-six-tenths shadow">play!</button>
+              </div>
+            </div>
+          </form>
+       </>
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -75,28 +75,27 @@ server.listen(process.env.PORT, () => {
 const nsDesktop = io.of('/desktop');
 
 nsDesktop.on('connection', socket => {
-  console.log('new desktop client connected:', socket.id);
-
   socket.on('create room', roomCode => {
-    console.log('room code created:', roomCode);
-
     socket.join(`room-${roomCode}`);
-    // console.log('rooms availabe:', nsDesktop.adapter.rooms);
   });
 });
 
 const nsMobile = io.of('/mobile');
 
 nsMobile.on('connection', socket => {
-  console.log('new mobile client connected:', socket.id);
   const { gameId } = socket.handshake.query;
+  const arr = Array.from(nsDesktop.adapter.rooms);
+  const validRooms = [];
+  for (let i = 0; i < arr.length; i++) {
+    validRooms.push(arr[i][0]);
+  }
   socket.on('create player', data => {
-
-    console.log('new player created:', data.name);
-
-    socket.join(`room-${gameId}`);
-
-    nsDesktop.to(`room-${gameId}`).emit('new player', data);
-
+    if (validRooms.includes(`room-${gameId}`)) {
+      socket.emit('valid id', true);
+      socket.join(`room-${gameId}`);
+      nsDesktop.to(`room-${gameId}`).emit('new player', data);
+    } else {
+      socket.emit('valid id', false);
+    }
   });
 });
